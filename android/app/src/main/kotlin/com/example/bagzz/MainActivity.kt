fa1c1519b5643b93bd97036d9c5276db9aa91a2d
Gérald Rindra RANAIVOSOA <rindra.ranaivosoa@paminnov.com>
2022-06-06 11:15:41 +0300

    initialisation and homepage structure set

---
+++ android/app/src/main/kotlin/com/example/bagzz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bagzz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/homePage.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // top bar
+
+
+              // cards scrollable horizontally : this season's latest
+
+
+              // Feedlist
+
+
+              // show by categories
+
+
+              // bottom bar
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
